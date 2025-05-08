19e4e55a8610f1562fc1a40fee775c17d9a130c7
Flack137 <denispancenko140@gmail.com>
2025-05-08 20:52:31 +0300

    Зробив admin.py, views.py, urls.py

--- Booking/views.py
+++ Booking/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Room, Booking, Availability
+
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'rooms/list.html', {'rooms': rooms})
+
+
+def booking_list(request):
+    bookings = Booking.objects.all()
+    return render(request, 'bookings/list.html', {'bookings': bookings})
+
+
+def availability_list(request):
+    availability = Availability.objects.all()
+    return render(request, 'availability/list.html', {'availability': availability})
 
 # Create your views here.
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Booking.views import room_list, booking_list, availability_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rooms/', room_list, name='room-list'),
+    path('bookings/', booking_list, name='booking-list'),
+    path('availability/', availability_list, name='availability-list'),
 ]
